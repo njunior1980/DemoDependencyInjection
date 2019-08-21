1f29a52450e3f3d0ad4482eb6936dccb91917fc2
Nelson Souza <njunior1980@gmail.com>
2019-08-21 01:33:19 +0100

    Commit

---
+++ DemoDependencyInjection/Startup.cs
@@ -0,0 +1,38 @@
+﻿using DemoDependencyInjection.Interfaces;
+using DemoDependencyInjection.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DemoDependencyInjection
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<IScopedService, ScopedService>();
+            services.AddSingleton<ISingletonService, SingletonService>();
+            services.AddTransient<ITransientService, TransientService>();
+
+            services.AddMvc()
+            .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc().UseMvcWithDefaultRoute();
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Demo Dependency Injection");
+            });
+        }
+    }
+}
